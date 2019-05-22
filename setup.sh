19e95cd5e4f08a4d52efca445f66ec92199cbb11
Swarles-Barkley <casters@oregonstate.edu>
2019-05-22 08:30:57 -0700

    cleanupcleanup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #hardcode password entry for alpines here?
-ssh-keygen -t rsa > ~/ssh/id_rsa
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.20
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.21
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.22
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.23
+yum install -y ansible
 
-ansible mynodes -i ./hosts.ini -m curl
-ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
-ansible mynodes -i ./hosts.ini -m ping
+ssh-keygen -t rsa
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-ansible-playbook webserver.yaml -i./hosts.ini
+ansible-playbook webserver.yaml -i ./hosts.ini
 
 curl 192.168.1.20:80
 curl 192.168.1.21:80
