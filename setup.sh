e3e04d645ac5b43866efee479e145f24ed7f2243
Swarles-Barkley <casters@oregonstate.edu>
2019-05-22 07:23:21 -0700

    curl module? might just use raw...

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.21
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.22
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.23
 
-curl 192.168.1.20:80
-curl 192.168.1.21:80
-curl 192.168.1.22:80
-curl 192.168.1.23:80
+#curl 192.168.1.20:80
+#curl 192.168.1.21:80
+#curl 192.168.1.22:80
+#curl 192.168.1.23:80
+
+ansible mynodes -i ./hosts.ini -m curl
+ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
+ansible mynodes -i ./hosts.ini -m ping
